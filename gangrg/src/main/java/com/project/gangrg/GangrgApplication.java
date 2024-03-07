00f6e0fcd53fc64d59ed88d3dd5de7f1d197e84d
junest66 <chlwndks333@gmail.com>
2024-03-07 14:54:57 +0900

    chore: Init Project 세팅
    #2

---
+++ gangrg/src/main/java/com/project/gangrg/GangrgApplication.java
@@ -0,0 +1,13 @@
+package com.project.gangrg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GangrgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GangrgApplication.class, args);
+	}
+
+}
---
+++ gangrg/src/test/java/com/project/gangrg/GangrgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.gangrg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GangrgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
