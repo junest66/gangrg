d874412d5728725636bf0c9113dff27975c3da86
junest66 <chlwndks333@gmail.com>
2024-03-23 21:08:58 +0900

    refactor: api 공통 반환객체로 변경

--- gangrg/src/main/java/com/project/gangrg/controller/UserController.java
+++ gangrg/src/main/java/com/project/gangrg/controller/UserController.java
@@ -1,12 +1,17 @@
 package com.project.gangrg.controller;
 
+import com.project.gangrg.common.ApiResponse;
 import com.project.gangrg.domain.User;
 import com.project.gangrg.dto.UserJoinRequest;
+import com.project.gangrg.dto.UserJoinResponse;
 import com.project.gangrg.service.JoinService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -17,9 +22,10 @@ public class UserController {
     private final JoinService joinService;
 
     @PostMapping("/api/v1/users")
-    public Long join(@RequestBody UserJoinRequest userJoinRequest) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse<UserJoinResponse> join(@Valid @RequestBody final UserJoinRequest userJoinRequest) {
         User user = joinService.signup(userJoinRequest);
-        return user.getId();
+        return ApiResponse.successResponse(UserJoinResponse.of(user));
     }
 
 }
